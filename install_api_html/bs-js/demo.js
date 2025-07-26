837ce7068f70b6831f4a08941827d93bf12fb412
THQ Cyber Division Unincorporated <thqcs@protonmail.com>
2025-07-27 00:37:02 +0200

    Add files via upload

---
+++ install_api_html/bs-js/demo.js
@@ -0,0 +1,57 @@
+﻿
+
+
+$(document).ready(function () {
+
+    $('.menu-item').hover(
+        function () {
+            $(this).parent().find('.menu-chevron').addClass('menu-chevron-rotate');
+            $(this).parent().addClass('dropdown-menu-active');
+        }
+    );
+
+    $('nav > div').hover(function () { },
+        function () {
+            $(this).find('.menu-chevron').removeClass('menu-chevron-rotate');
+            $(this).removeClass('dropdown-menu-active');
+        }
+    );
+
+
+    if ($('#dropdown_menu').length) {
+        $.post('https://api.bizstarter.us/api/buildservicemenu/', function(response) {
+            $('#dropdown_menu').html(response);
+            $('#mobile_menu').html(response);
+        });
+    }
+
+
+
+    $('.burger').on('click', function () {
+        $('.mobile-menu').fadeIn();
+    });
+
+    $('.mobile-menu_close img').on('click', function () {
+        $('.mobile-menu').fadeOut();
+    });
+
+
+    if ($('#bizstarter_addinional-services-container').length) {
+        $.post('https://api.bizstarter.us/setting/getsettingbyid/?id=1', function (response) {
+            $('#bizstarter_addinional-services-container').html(response);
+        });
+    }
+
+    if ($('#bizstarter_page_container').length) {
+        let page_id = getUrlValue('page_id');
+
+        if (page_id != '') {
+            $.post('https://api.bizstarter.us/setting/getsettingbyid/?id=' + page_id, function (response) {
+                $('#bizstarter_page_container').html(response);
+            });
+        }
+    }
+
+});
+
+
